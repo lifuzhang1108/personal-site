ecd2cccc9a29adc256697b419198721cf73cbb53
Alex Zhang <1309701864@qq.com>
2021-01-30 18:47:09 -0500

    init

---
+++ src/data/about.md
@@ -0,0 +1,7 @@
+
+# Intro
+
+I am Lifu Zhang, a third-year student studying computer engineering at BU. I have numerous CS-related research and industrial experience. I am open to any ML related research positions as well as part-time SDE position.
+# Currently
+
+I have 3 months of internship experience on the back-end for a logistics provider company. As a software engineer, I implemented a data desensitization System using Java and Spring MVC based framework. I am always passionate to design and develop personal projects in my spare time. Such as a Smart Messenger Android App, which supports navigation between different chat platforms. Besides software development, I am also passionate about machine learning research. I have worked with BU medical school to predict Dementia disease using both Computer Vision and Natural Language Processing techniques. Currently, I am working as a research assistant at the Harvard Lichtman Lab to segment 3D brain images using U-NET, our group is also collaborating with Google Brain for this particular project.
---
+++ src/data/projects.js
@@ -0,0 +1,39 @@
+// TODO Add a couple lines about each project
+const data = [
+  {
+    title: 'Deep Learning for dementia prediction',
+    subtitle: '2015 BVP Hackathon',
+    link: 'https://www.bu.edu/ihsip/2020/09/14/measuring-risk-for-dementia-with-deep-learning/',
+    image: '/images/projects/dcdt.png',
+    date: '2020-09-20',
+    desc:
+        'This research project is conducted with BU Framingham Study Group. '
+        + 'We leverage the Clock Drawing Test (CDT)'
+        + 'to develop an diagnostic model.',
+  },
+  {
+    title: 'All-in-One Messenger APP',
+    subtitle: '2015 BVP Hackathon',
+    link: 'https://github.com/lifuzhang1108/Smart-Messenger-App',
+    image: '/images/projects/messenger.png',
+    date: '2019-07-20',
+    desc:
+        'Scintaxx is an android studio app that integrates '
+        + 'all your messaging apps into one app.'
+        + 'You can reply to SMS messages, Emails, '
+        + 'and even Facebook messages through Scintaxx.',
+  },
+  {
+    title: 'Course Calendar Tool',
+    subtitle: '2015 BVP Hackathon',
+    link: 'https://github.com/lifuzhang1108/Generate-Schedule-on-Google-calender',
+    image: '/images/projects/calendar.png',
+    date: '2019-11-20',
+    desc:
+      'Built for generating course calendar. '
+      + 'reading course schedule in csv format and output in Google calendar, '
+      + 'compatible format ',
+  },
+];
+
+export default data;
